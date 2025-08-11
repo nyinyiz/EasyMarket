2f92f0d4d3f8e287837920b4c09b844351af856d
Nyi Nyi Zaw <nyi@passkit.com>
2025-08-11 15:42:03 +0700

    feat: Add Ktor HttpClient with ContentNegotiation and Logging

---
+++ composeApp/src/commonMain/kotlin/com/nnzapp/easymarket/data/remote/HttpClient.kt
@@ -0,0 +1,27 @@
+package com.nnzapp.easymarket.data.remote
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+fun createHttpClient(): HttpClient {
+    return HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+        
+        install(Logging) {
+            logger = Logger.SIMPLE
+            level = LogLevel.ALL
+        }
+    }
+}
